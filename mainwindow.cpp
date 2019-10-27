6843b3229e2f743bc54e9bed3f09ceee90be2c67
JackLovel <gogkatsu@outlook.com>
2019-10-27 20:57:47 +0800

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    createQipan(3, 2);
+
 }
 
 MainWindow::~MainWindow()
@@ -19,12 +19,15 @@ void MainWindow::paintEvent(QPaintEvent *e)
 {
     Q_UNUSED(e)
 
-    createQipan(4, 4);
+    // 15 x 15 的棋盘
+    createQipan(15, 15);
 }
 
 
 void MainWindow::createQipan(int colCount, int rowCount)
 {
+    // 自动生成网格
+
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
     painter.setPen(QPen(Qt::black));
@@ -32,8 +35,8 @@ void MainWindow::createQipan(int colCount, int rowCount)
 
     // 3 x 3
     int sideLength = 40; // 每个网格的边长
-    int posX = 0, posY = 0;
-    int startX = 20, startY = 20;
+    int posX = 0, posY = 0; // 每个网格最左上方的坐标
+    int startX = 20, startY = 20; // 整个网格的左上方的坐标
     for (int i = 0; i < colCount; i++) {
         for (int j = 0; j < rowCount; j++) {
             posX = startX + i * sideLength;
